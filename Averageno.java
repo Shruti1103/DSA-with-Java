b1050f9ce2758bbadba1c31015dd1baacde0f44f
Shruti <kansalshruti096@gmail.com>
2024-02-18 17:27:45 +0530

    Adavance Patterns Java Patterns

---
+++ Averageno.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Averageno {
+    public static int average(int n1,int n2,int n3){
+        int avg = (n1 + n2 + n3)/3;
+        return avg;
+    }
+
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no.s");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+        int average = average(a,b,c);
+        System.out.println("Average of three no. is : "+average);
+    }
+}
---
+++ Invertednumberpyramid.java
@@ -0,0 +1,14 @@
+public class Invertednumberpyramid {
+    public static void numberpyramid(int n){
+        int i,j;
+      for(i=1;i<=n;i++){
+        for(j=1;j<=n-i+1;j++){
+            System.out.print(j);
+        }
+        System.out.println(" ");
+      }
+    }
+    public static void main(String[] args){
+       numberpyramid(5);
+    }
+}
